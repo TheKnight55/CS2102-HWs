b544d6012345c084f9f41cb7c54b561bc57c582d
caterinaCorapi <ce.corapi@gmail.com>
2025-11-03 23:01:02 -0500

    Update Battery.java

--- src/Battery.java
+++ src/Battery.java
@@ -2,11 +2,20 @@ public class Battery {
     /** maximum power provided by the battery (when full) in milliAmpere-
     seconds (mAs or mA-s) */
     double capacity;
+    
     /** current power providable by a battery in use in milliAmpere-
      seconds (mAs or mA-s) */
     double amountLeft;
+
+    /**
+     * The constructor for a battery assuming it can draw any amount of current without exploding
+     * @param capacity is the maximum power provided by the battery (when full) in milliAmpere-
+     * seconds (mAs or mA-s)
+     * @param amountLeft is the current power providable by a battery in use in milliAmpere-seconds
+     * (mAs or mA-s)
+     */
     public Battery(double capacity, double amountLeft) {
-        this.capacity=capacity;
-        this.amountLeft=amountLeft;
+        this.capacity = capacity;
+        this.amountLeft = amountLeft;
     }
 }
